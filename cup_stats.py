797a5e003ab427ca7db333587143dde5340e63da
Charles Arthur <charles.v.arthur@gmail.com>
2023-01-01 13:36:55 +0000

    test full stats dataframe

--- cup_stats.py
+++ cup_stats.py
@@ -21,3 +21,9 @@ scoring = ('Stablesford scoring system is as follows:<br>'
         '<li><strong>4 Points</strong> - Eagle or higher</li></ul>')
 
 st.markdown(scoring,unsafe_allow_html=True) 
+
+#Main data source
+full_stats = pd.DataFrame('https://raw.githubusercontent.com/charlesvarthur/Golf_Tournament/main/cup_full_stats.csv')
+st.write(full_stats.head(5))
+
+#Figure 1 - Leaderboard Table
