d7c38d7c0fcb11f42a6b1a72e633a32af1a4e9c9
Charles Arthur <charles.v.arthur@gmail.com>
2023-01-01 15:06:57 +0000

    undo last edit

--- cup_stats.py
+++ cup_stats.py
@@ -49,7 +49,6 @@ full_with_stableford['stableford_score'] = stableford
 player_scores = full_with_stableford.loc[:,['player_id','score','stableford_score']].groupby(by=['player_id']).sum().reset_index()
 player_scores = player_scores.sort_values(by=['stableford_score'], ascending=False)
 player_scores.set_axis(['Player ID', 'Stroke Score', 'Stableford Score'], axis='columns', inplace=True)
-player_scores_tb = player_scores['Player ID', 'Stroke Score', 'Stableford Score']
 
 st.subheader('League Table')
-st.write(player_scores_tb)
+st.write(player_scores)
