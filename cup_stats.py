638192fc0d21fbad3512b735fd7a8e5b52e7eccf
Charles Arthur <charles.v.arthur@gmail.com>
2023-01-01 13:17:33 +0000

    set list as a variable with markdown to run

--- cup_stats.py
+++ cup_stats.py
@@ -12,9 +12,12 @@ st.header('Sadomasochism Cup')
 st.write('Welcome to the stats page for the Sadomasochism Cup.'
         ' Here you will find stats and statistics for head player, which will be updated throughout the tournament.')
 
-st.markdown('Stablesford scoring system is as follows:'
+
+scoring = ('Stablesford scoring system is as follows:<br>'
         '<ul><li> 0 Points - Double Bogey or higher</li>'
         '<li> 1 Point - Bogey</li>'
         '<li> 2 Points - Par</li>'
         '<li> 3 Points - Birdie</li>'
         '<li> 4 Points - Eagle or higher</li></ul>')
+
+st.markdown(scoring)
