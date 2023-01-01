92b755b690c74535c73a4713b173816c3dcdfb62
Charles Arthur <charles.v.arthur@gmail.com>
2023-01-01 16:24:03 +0000

    rename columns

--- cup_stats.py
+++ cup_stats.py
@@ -51,7 +51,7 @@ full_with_stableford['stableford_score'] = stableford
 
 
 player_scores = full_with_stableford.loc[:,['player_id','score','stableford_score']].groupby(by=['player_id']).sum()
-player_scores.rename(columns={'sum': 'Player ID'})
+player_scores.rename(columns={'sum':'Player ID','score':'Stroke Score', 'stableford':'Stableford Score'})
 player_scores = player_scores.sort_values(by=['stableford_score'], ascending=False)
 player_scores.set_axis(['Stroke Score', 'Stableford Score'], axis='columns', inplace=True)
 
