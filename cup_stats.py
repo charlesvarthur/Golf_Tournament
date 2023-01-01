501eb76a2c9002b2fc75fb3e460bb0b70896442f
Charles Arthur <charles.v.arthur@gmail.com>
2023-01-01 13:11:24 +0000

    Test unordered list

--- cup_stats.py
+++ cup_stats.py
@@ -12,4 +12,9 @@ st.header('Sadomasochism Cup')
 st.write('Welcome to the stats page for the Sadomasochism Cup.'
         ' Here you will find stats and statistics for head player, which will be updated throughout the tournament.')
 
-
+st.write('Stablesford scoring system is as follows:'
+        '<ul><li> 0 Points - Double Bogey or higher</li>'
+        '<li> 1 Point - Bogey</li>'
+        '<li> 2 Points - Par</li>'
+        '<li> 3 Points - Birdie</li>'
+        '<li> 4 Points - Eagle or higher</li></ul>')
