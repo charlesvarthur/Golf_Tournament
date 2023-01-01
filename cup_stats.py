6d362dc8add5dead06b67c5c393d4224980e077c
Charles Arthur <charles.v.arthur@gmail.com>
2023-01-01 13:06:17 +0000

    first commit

---
+++ cup_stats.py
@@ -0,0 +1,15 @@
+#####################
+# Sadomasochism Cup #
+#####################
+
+import pandas as pd
+import numpy as np
+import streamlit as st
+import altair as alt
+
+
+st.header('Sadomasochism Cup')
+st.write('Welcome to the stats page for the Sadomasochism Cup.'
+        ' Here you will find stats and statistics for head player, which will be updated throughout the tournament.')
+
+
