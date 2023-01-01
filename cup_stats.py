187f2450d73e8da0c8993456e66e0e291b2cce7f
Charles Arthur <charles.v.arthur@gmail.com>
2023-01-01 13:16:05 +0000

    changed write to markdown for list

--- cup_stats.py
+++ cup_stats.py
@@ -12,7 +12,7 @@ st.header('Sadomasochism Cup')
 st.write('Welcome to the stats page for the Sadomasochism Cup.'
         ' Here you will find stats and statistics for head player, which will be updated throughout the tournament.')
 
-st.write('Stablesford scoring system is as follows:'
+st.markdown('Stablesford scoring system is as follows:'
         '<ul><li> 0 Points - Double Bogey or higher</li>'
         '<li> 1 Point - Bogey</li>'
         '<li> 2 Points - Par</li>'
